b86c476ebfe16406066d2da943c929cf36a583fc
teamd <teamdlok@gmail.com>
2025-03-27 14:15:27 +1000

    Added first serializer

---
+++ women/serializers.py
@@ -0,0 +1,34 @@
+import io
+
+from rest_framework import serializers
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+
+from women.models import Women
+
+class WomenModel:
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
+
+class WomenSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+
+
+def encode():
+    """Тут мы сделали базовое преобразование информации из базы данных в json байтовую строку"""
+    model = WomenModel('Angelina Jolie', 'Text for Agnelina Jolie')
+    model_sr = WomenSerializer(model)
+    print(model_sr.data, type(model_sr.data), sep="\n")
+    json = JSONRenderer().render(model_sr.data)
+    print(json)
+
+def decode():
+    """Тут мы докедируем байтовую json строку преобразуя в словарь python"""
+    stream = io.BytesIO(b'{"title":"Angelina Jolie","content":"Text for Agnelina Jolie"}')
+    data = JSONParser().parse(stream=stream)
+    serializer = WomenSerializer(data=data)
+    serializer.is_valid()
+    print(serializer.validated_data)
